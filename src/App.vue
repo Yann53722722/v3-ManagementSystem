da9444dea273a3b9a7e54d9782291084236e9dc9
yanjs <MS_yjs@outlook.com>
2026-05-11 18:22:36 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>You did it!</h1>
+  <router-view></router-view>
+</template>
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      component: () => import('../views/login.vue'),
+    },
+    {
+      path: '/login',
+      redirect: '/',
+    },
+    {
+      path: '/index',
+      component: () => import('../views/index.vue'),
+    },
+  ],
+})
+
+export default router
