1f1afe3079ee11e08480a210325b86a0991709d2
yanjs <MS_yjs@outlook.com>
2026-06-01 17:30:38 +0800

    feat: 规范登录接口，并添加部分登出接口

--- mock/auth.ts
+++ mock/auth.ts
@@ -1,8 +1,10 @@
 import { MockMethod } from 'vite-plugin-mock'
 import { decrypt } from '../src/utils/forgeEncrypt'
+import { getToken, removeToken } from '../src/utils/auth'
 
 export default [
   {
+    // 登录
     url: '/api/login',
     method: 'post',
     timeout: 1000, // 模拟网络延迟
@@ -20,6 +22,26 @@ export default [
         return {
           code: 500,
           msg: '账号或密码错误',
+        }
+      }
+    },
+  },
+  {
+    // 登出
+    url: '/api/logout',
+    method: 'post',
+    timeout: 500,
+    response: () => {
+      if (getToken()) {
+        removeToken() // 移除token
+        return {
+          code: 200,
+          msg: '登出成功',
+        }
+      } else {
+        return {
+          code: 401,
+          msg: '未登录',
         }
       }
     },
--- src/api/login.ts
+++ src/api/login.ts
@@ -1,4 +1,4 @@
-import request from '@/utils/request'
+import { post } from '@/utils/request' // 注意使用具名导入，使用封装的方法
 import { encrypt } from '@/utils/forgeEncrypt'
 
 // 登录接口的参数类型
@@ -6,8 +6,18 @@ export interface LoginData {
   userId: string
   password: string
 }
+// 登录接口返回的数据类型
+export interface LoginResponse {
+  token: string
+}
+
 // 登录接口
-export const login = (data: LoginData): Promise<any> => {
+export const login = (data: LoginData): Promise<LoginResponse> => {
   data.password = encrypt(data.password) // 加密密码
-  return request.post('/api/login', data)
+  return post<LoginResponse>('/api/login', data)
+}
+
+// 登出接口，不需要传参，后端根据token识别用户身份并进行登出处理
+export const logout = (): Promise<void> => {
+  return post<void>('/api/logout')
 }
